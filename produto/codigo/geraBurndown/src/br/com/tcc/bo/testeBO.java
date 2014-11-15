d57c7143e1d823014bbb68999c631cebb86ee087
NaiaraMagna <naiaracead@gmail.com>
2014-11-15 20:15:06 -0200

    Teste de commit
    teste de conecção

---
+++ produto/codigo/geraBurndown/src/br/com/tcc/bo/testeBO.java
@@ -0,0 +1,14 @@
+package br.com.tcc.bo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testeBO {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
